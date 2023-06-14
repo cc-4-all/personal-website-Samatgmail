1a858ad774ce33f88cc0022a6e04c25c7a147a0a
Samatgmail <136475691+Samatgmail@users.noreply.github.com>
2023-06-14 15:50:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-let pic = document.getElementById("profile-picture");
+var img = document.getElementById("gallery-img");
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
 
-document.addEventListener("scroll", function () {
-  if (window.scrollY > 5) {
-    pic.style.borderRadius = "0%";
-  } else {
-    pic.style.borderRadius = "100%";
-  }
-});
+var images = ["mario3.png", "ai.png", "astro-bot.png", "violin.png", "yoshi.png", "violin-bow.png", "pc.png", "bike.png", "bike-lock.png", "cycling-helmet.png", "hurling-helmet.png", "hurley.png", "gaelic-football.png", "link.png", "monitor.png", "mouthguard.png", "mr-krabs.png", "music-notes.png"]
+
+var currentimage = 0;
+
+function changeimage(e) {
+    if (e.target.id == "next" && currentimage !== images.length - 1){
+        currentimage = currentimage + 1;
+    }
+    
+    
+
+
+if (e.target.id == "prev" && currentimage !== 0) {
+
+    currentimage = currentimage - 1;
+}
+
+img.setAttribute("src", "images/" + images[currentimage]);
+
+}
+
+
+
+
+prev.addEventListener("click", changeimage)
+next.addEventListener("click", changeimage)
